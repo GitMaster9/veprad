ca9a65d410b7bd8cd86be30169d020dc8b3aa6d6
GitMaster9 <karlosecen10@gmail.com>
2024-06-30 22:08:41 +0200

    added converting dict from merlin

---
+++ convert_dict.py
@@ -0,0 +1,21 @@
+import re
+
+# Open the input file and read the content
+with open('nes.txt', 'r') as file:
+    lines = file.readlines()
+
+# Process each line
+modified_lines = []
+for line in lines:
+    columns = re.split(r'\t+', line.strip())  # Split the line into columns based on 1 to 3 tabs
+    if len(columns) == 2:  # Ensure there are exactly 2 columns
+        first_col = columns[0]
+        second_col = columns[1]
+        new_col = f'[{first_col}]'
+        modified_line = f'{first_col}\t{new_col}\t{second_col}'
+        modified_lines.append(modified_line)
+
+# Write the modified content to a new file
+with open('converted_dict.txt', 'w') as file:
+    for modified_line in modified_lines:
+        file.write(modified_line + '\n')
